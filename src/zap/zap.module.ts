1914c381193ab08c244f6252a1e81753c048f156
Matheus Morais <matheus.silva.morais01@gmail.com>
2023-06-22 14:01:37 -0300

    fix: adicionando o modulos http e config

--- src/zap/zap.module.ts
+++ src/zap/zap.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ZapService } from './zap.service';
 import { ZapController } from './zap.controller';
+import { HttpModule } from '@nestjs/axios';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
+  imports: [HttpModule, ConfigModule.forRoot()],
   providers: [ZapService],
-  controllers: [ZapController]
+  controllers: [ZapController],
 })
 export class ZapModule {}
